468825f8cc9b3861fbb27851c99ca379c8e96d1a
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-07-03 07:58:56 -0500

    Modificacion de los archivos para agregar funcionalidad

--- src/data.js
+++ src/data.js
@@ -1,68 +1,96 @@
+window.computeStudentsStats = (laboratoria) => {
+    const student = [];
+    let array2 = [];
+    let name = '';
+    let email = '';
+    let campus = '';
+    let stats = [];
+    let status = 0;
+    let completedPercentage = 0;
+    let topics = [];
+    let percentageDuration = 0;
+    let subtopics = [];
+    let type = '';
+    let duration = 0;
 
-//window.data = {
-
-const fillSelect = ()=> {
-    let link = "https://alterna87.github.io/cdmx-2018-06-bc-core-am-data-dashboard/data/laboratoria.json";
-       fetch(link).then(function(datos){
-          //console.log(datos);
-        return datos.json();
-        }).then(function(data){
-        let sedes = document.getElementById('sedes')
-        for (let i = 0; i < Object.keys(data).length; i++) {
-        sedes.options[i] = new Option(Object.keys(data)[i]);
-        }
-        sedeGeneracion = sedes.selectedIndex;
-        console.log(sedeGeneracion);
-        sedes.addEventListener('change', generacion =()=> {
-        let sedeGeneracion= sedes.value;
-        let generaciones = document.getElementById('generaciones');
-        switch (sedeGeneracion) {
-
-          case 'lima':
-          for (var i = 0; i < Object.keys(data.lima.generacion).length; i++) {
-            generaciones.options[i] = new Option(Object.keys(data.lima.generacion)[i]);
-          }
-          case 'mexico':
-          for (var i = 0; i < Object.keys(data.mexico.generacion).length; i++) {
-            generaciones.options[i] = new Option(Object.keys(data.mexico.generacion)[i]);
+    for (key in laboratoria) {
+        campus = key;
+        average = 0;
+        const generations = Object.keys(laboratoria[key].generacion);
+        generations.map((generation) => {
+            generation = generation; 
+            const students = laboratoria[key].generacion[generation].estudiantes;
+            array2 = students.map((student, i, array) =>{
+                name = array[i].nombre;
+                email = array[i].correo;
+                completedPercentage = array[i].progreso.porcentajeCompletado;
+                //status =;
+                const progress = array[i].progreso;
+                console.log(progress);                
+                //array3 = progress.map((temas, j, cadena) =>{
+                    //percentageDuration = cadena[j].duracionTemaCompletado;
+                    //console.log(percentageDuration);
+                    return {
+                        'nombre' : name,
+                        'correo' : email,
+                        'sede' : campus,
+                        'generacion' : generation,
+                        stats : {
+                            'status' : status,
+                            'Pocentaje completado' : completedPercentage,
+                            topics : {
+                                //'Pocentaje completado' : completedPercentage,
+                                'Tiempo invertido' : percentageDuration + ' horas.',
+                                subtopics : {
+                                    //'Pocentaje completado' : completedPercentage,
+                                    'Tipo' : type,
+                                    'Duracion' : duration
+                                }
+                            }
+                        }
+                    }
+                //})   
+            })
+            student.push(array2)
+        })        
+    }
+    return student
 }
-          case 'santiago':
-          for (var i = 0; i < Object.keys(data.santiago.generacion).length; i++) {
-          generaciones.options[i] = new Option(Object.keys(data.santiago.generacion)[i]);
+
+window.computeGenerationsStats = (laboratoria) => {
+    const generationsArray = [];
+
+    let array2 = [];
+    let average = 0;
+    let campus;
+    let generation;
+    for (key in laboratoria) {
+        campus = key;
+        average = 0;
+        const generations = Object.keys(laboratoria[key].generacion);
+        generations.map((generation) => {
+            generation = generation; 
+            const students = laboratoria[key].generacion[generation].estudiantes;
+            array2 = students.map((student, i, array) =>{
+                average += array[i].progreso.porcentajeCompletado;
+                average = Math.round(average / array.length);
+                return {
+                    'campus' : key,
+                    'generation' : generation,
+                    'average' : average,
+                    'count': array.length
+                }                
+            })
+            generationsArray.push(array2)
+        })        
+    }    
+    return generationsArray
 }
-            break;
-          default:
-        }
 
-      /*  for (let i = 0; i < data.sedeGeneracion.length; i++) {
+window.sortStudents = (students, orderBy, orderDirection) =>{
 
-      }*/
-        })
-        /*for (let i = 0; i < data.sedeGeneracion.length; i++) {
-          array[i]
-        */
-        //console.log(sedes.value);
+}
 
+window.filterStudents = (students, search) =>{
 
-      });
-      }
-
-
-fillSelect();
-
-//};
-//fillSelect();
-
-
-
-
-
-/*sedes.addEventListener("click", sede =()=> {
-let c = document.createElement("option");
-
-
-//  sedes.options[0] = new Option();
-//alert("Click");
-//Object.keys(data).length
-//7Object.keys(data)[0]
-});*/
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
+const getData = () => {
+    fetch('https://api.myjson.com/bins/nslqe')
+    .then(laboratoria => laboratoria.json())
+    .then((laboratoria) => {
+        const generations = computeGenerationsStats(laboratoria);
+        const users = computeStudentsStats(laboratoria);
+        console.log(generations);
+        console.log(users);
+        drawCampus(generations);
+        drawGenerations(users);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+getData();
+
+const drawCampus = (data) => {
+    const sedes = Object.keys(data);
+
+    const containerCampus = document.getElementById('campus');
+    sedes.forEach((sede) => {
+        const option = document.createElement('option');
+        option.innerHTML = sede;
+        containerCampus.appendChild(option);
+    });
+
+    containerCampus.addEventListener('change', drawCampus);
+};
+
+
+const drawGenerations = (e) => {
+    const stats =  Object.keys(e);
+
+    const containerStats = document.getElementById('campus2');
+    stats.forEach((stat) => {
+        const option = document.createElement('option');
+        option.innerHTML = stat;
+        containerStats.appendChild(option);
+    });
+
+    containerStats.addEventListener('change', drawGenerations);
+};
 
